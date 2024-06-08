def84ddae30114cdfc89736dcf22e093076ad1e9
AxelADN <33737297+AxelADN@users.noreply.github.com>
2024-06-08 01:29:21 +0000

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,44 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn],null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let author = req.params.author;
+    let books_list = Object.values(books);
+    books_list.forEach( book => {
+        if(author == book["author"]){
+            return res.send(JSON.stringify(book,null,4));
+        }
+    });
+    return res.status(300).json({message: "Author not in DataBase"});
+    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title;
+    let books_list = Object.values(books);
+    books_list.forEach( book => {
+        if(title == book["title"]){
+            return res.send(JSON.stringify(book,null,4));
+        }
+    });
+    return res.status(300).json({message: "Title not in DataBase"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    return res.send(JSON.stringify(books[isbn]["reviews"],null,4));
 });
 
 module.exports.general = public_users;
